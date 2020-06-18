c09dd81be4efeef208568f2d1a219176c0687b81
longbvntp <long.bvntp@gmail.com>
2020-06-18 11:20:02 +0700

    Add TestController

---
+++ Controllers/TestController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class TestController : Controller
+    {       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string a = "asdfasdf";
+            ViewBag.a = a;
+            return View();
+        }
+        
+    }
+}
